7385f8d7ab82cbf5457a7f950295320658481c3a
afrodancer <jwboone27@gmail.com>
2020-11-11 17:19:08 -0800

    Initial commit

---
+++ Assets/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float xSpeed;
+    public float xIncr;
+    public float xMaxSpeed;
+    public float jumpSpeed;
+    public float gravity;
+    private Vector3 moveVector;
+
+    void Start()
+    {
+        //enables reading input from player
+        controller = GetComponent<CharacterController>();
+        xSpeed = 0f;
+        xIncr = 1.1f;
+        xMaxSpeed = 7.5f;
+        jumpSpeed = 2.5f;
+        gravity = -18f;
+        moveVector = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //x-axis=red/transform.right, y-axis=green/transform.up, z-axis=blue/transform.forward
+
+    }
+}

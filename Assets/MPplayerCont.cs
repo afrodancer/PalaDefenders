db114cfe10f73eb2dd1cbd344352f8f4d7728475
afrodancer <jwboone27@gmail.com>
2020-11-14 12:14:50 -0800

    switched movement code
    went back to using the movement example that the teacher gave us, while setting aside the one I was working on

---
+++ Assets/MPplayerCont.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MPplayerCont : MonoBehaviour
+{
+    Rigidbody rbody;
+    public LayerMask ground;
+    public Transform feet;
+
+    public float jumpHeight;
+    private Vector3 direction;
+    private float moveSpeed;
+    private float doubleJump;
+    private float maxJumps;
+    public float gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        jumpHeight = 20f;
+        maxJumps = 1f;
+        moveSpeed = 20f;
+        doubleJump = 0f;
+        gravity = -2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction = direction.normalized;
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+            rbody.MovePosition(transform.position + direction * moveSpeed * Time.fixedDeltaTime);
+        }
+        bool isGrounded()
+        {
+            if (Physics.CheckSphere(feet.position, 0.1f, ground))
+            {
+                doubleJump = 0;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && (isGrounded() || doubleJump < maxJumps))
+        {
+            doubleJump += 1;
+            rbody.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+        }
+        rbody.AddForce(Vector3.up * gravity, ForceMode.Acceleration);
+
+    }
+
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+	Rigidbody rbody;
+	public LayerMask ground;
+	public Transform feet;
+
+	private float jumpHeight;
+	private Vector3 direction;
+	private float moveSpeed;
+	private float maxSpeed;
+	private float threshold;
+	private float decelerate;
+	private float doubleJump;
+	private float maxJumps;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		rbody = GetComponent<Rigidbody>();
+		jumpHeight = 3f;
+		maxJumps = 1f;
+		moveSpeed = 8f;
+		maxSpeed = 10f;
+		threshold = 0.05f;
+		direction = Vector3.zero;
+		decelerate = -0.4f;
+		doubleJump = 0f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        bool isGrounded()
+        {
+            if (Physics.CheckSphere(feet.position, 0.1f, ground))
+            {
+                doubleJump = 0;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        if (Input.GetButtonDown("Jump") && (isGrounded() || doubleJump < maxJumps))
+        {
+            doubleJump += 1;
+            rbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+			//BUG: jumping causes player to start accelerating backwards
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+			direction.x = Input.GetAxis("Horizontal");
+			direction = direction.normalized;
+			if (direction != Vector3.zero)
+			{
+				transform.forward = direction;
+				if (rbody.velocity.magnitude > maxSpeed)
+				{ return; }
+				else
+				{
+					rbody.AddForce(direction * moveSpeed, ForceMode.Acceleration); 
+				}
+			}
+        }
+        else
+        {
+            if (rbody.velocity.magnitude > threshold)
+            {
+				rbody.AddForce(direction * rbody.velocity.magnitude * decelerate, ForceMode.Acceleration);
+			}
+        }
+
+    }
+}
